0b624399f6a7d7a9f53f0b64569a4f2f9e5a833a
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-21 21:19:08 +0530

    created CANDIDATE table migration

---
+++ migrations/20200721154449-create-table-candidate.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, cb) {
+  db.runSql('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+  db.createTable('CANDIDATE', {
+    id: {
+      type: 'int',
+      unsigned: true,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    guid: {
+      type: 'uuid',
+      unique: true,
+      notNull: true
+    },
+    displayHeader: {
+      type: 'string',
+      maxLength: 500,
+      notNull: true
+    },
+    summary: {
+      type: 'string'
+    },
+    candidateStatus: {
+      type: 'string',
+      notNull: true,
+      defaultValue: 'ACTIVE'
+    },
+    createdAt: {
+      type: 'timestamptz',
+      notNull: true
+    },
+    updatedAt: {
+      type: 'timestamptz'
+    }
+  }, cb);
+};
+
+exports.down = function (db, cb) {
+  db.dropTable('CANDIDATE', {}, cb);
+};
+
+exports._meta = {
+  'version': 1
+};
