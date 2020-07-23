cfe15b14e9ca227d6cc320bbeb92806305704f06
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-23 12:16:22 +0530

    update

--- tests/unit/app/services/log-service.unit.test.js
+++ tests/unit/app/services/log-service.unit.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {keys} = require('lodash');
+const { keys } = require('lodash');
 const ConfigService = require('../../../../app/services/config-service.js');
 const LogService = require('../../../../app/services/log-service.js');
 
@@ -8,11 +8,11 @@ const fakePinoLogger = () => ({
   info: (...v) => v,
   success: (...v) => v,
   error: (...v) => v,
-  verbose: (...v) => v,
+  verbose: (...v) => v
 });
 const configService = ConfigService();
 const logService = LogService(configService, fakePinoLogger);
-const {logger, INFO, SUCCESS, ERROR, VERBOSE} = logService;
+const { logger, INFO, SUCCESS, ERROR, VERBOSE } = logService;
 
 test('Should confirm the properties of logger', () => {
   expect(keys(logger).sort()).toStrictEqual(['error', 'info', 'success', 'verbose'].sort());
