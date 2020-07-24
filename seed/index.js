216c2ab123490830fab8a88ed4ee983cc858cdee
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-24 22:12:53 +0530

    seed refactor

--- seed/index.js
+++ seed/index.js
@@ -5,24 +5,24 @@ const Mappers = require('../app/mappers');
 const Repositories = require('../app/repositories');
 const mappers = new Mappers();
 const services = new Services();
-const {knexService: knex, logService: {INFO, ERROR, SUCCESS}} = services;
-const repositories = new Repositories(mappers);
-const {countryRepository} = repositories;
+const {
+  knexService: knex,
+  logService: { INFO, ERROR },
+  configService
+} = services;
+const repositories = new Repositories(mappers, configService);
+const { countryRepository } = repositories;
 
 function upsertCountries(countries, txn) {
   return countries.map(country => countryRepository.upsert(country, txn));
 }
 
 async function run() {
-  try {
-    return knex.transaction(async txn => {
-      const countries = require('./country.js');
-      INFO('Inserting countries...');
-      await Promise.all(upsertCountries(countries, txn));
-    });
-  } catch (err) {
-    throw err;
-  }
+  return knex.transaction(async txn => {
+    const countries = require('./country.js');
+    INFO('Inserting countries...');
+    await Promise.all(upsertCountries(countries, txn));
+  });
 }
 
 run()
