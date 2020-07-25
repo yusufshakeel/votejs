9784c9073a8820ffc9844ba0b610811f9d1fc854
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-25 17:00:16 +0530

    code refactor

--- app/repositories/country-repository.js
+++ app/repositories/country-repository.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const { isEmpty, first } = require('lodash');
+const {
+  select: selectQuery,
+  insert: insertQuery,
+  update: updateQuery
+} = require('../functional/query.js');
 const TableRepository = require('./table-repository.js');
 const tableRepository = new TableRepository();
 const T = tableRepository.tables();
@@ -10,30 +15,48 @@ const columnsToReturn = ['countryCode', 'countryName', 'code'];
 function CountryRepository(mappers) {
   const { countryMapper } = mappers;
 
+  const findBy = params => selectQuery({ table: T.COUNTRY, ...params });
+
   this.create = async function (domainCountry, transaction) {
     const dbCountry = countryMapper.domainToDb(domainCountry);
-    const result = await transaction(T.COUNTRY).insert(dbCountry).returning(columnsToReturn);
+    const result = await insertQuery({
+      table: T.COUNTRY,
+      dataToInsert: dbCountry,
+      columnsToReturn,
+      transaction
+    });
     return countryMapper.dbToDomain(first(result));
   };
 
   this.findByCountryCode = async function (countryCode, transaction) {
-    const result = await transaction(T.COUNTRY).select(columnsToReturn).where({ countryCode });
+    const result = await findBy({
+      whereClause: { countryCode },
+      columns: columnsToReturn,
+      transaction
+    });
     if (isEmpty(result)) return null;
     return countryMapper.dbToDomain(first(result));
   };
 
   this.findByCode = async function (code, transaction) {
-    const result = await transaction(T.COUNTRY).select(columnsToReturn).where({ code });
+    const result = await findBy({
+      whereClause: { code },
+      columns: columnsToReturn,
+      transaction
+    });
     if (isEmpty(result)) return null;
     return countryMapper.dbToDomain(first(result));
   };
 
   this.updateByCountryCode = async function (countryCode, domainCountry, transaction) {
     const dbCountry = countryMapper.domainToDb(domainCountry);
-    const result = await transaction(T.COUNTRY)
-      .update(dbCountry)
-      .where({ countryCode })
-      .returning(columnsToReturn);
+    const result = await updateQuery({
+      table: T.COUNTRY,
+      dataToUpdate: dbCountry,
+      whereClause: { countryCode },
+      columnsToReturn,
+      transaction
+    });
     return countryMapper.dbToDomain(first(result));
   };
 
