8bfb6c97dac9fe5d6ede0f863e03a159d8651fed
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-26 22:18:04 +0530

    repositories added

--- app/repositories/index.js
+++ app/repositories/index.js
@@ -3,11 +3,20 @@
 const CountryRepository = require('./country-repository.js');
 const AdminRepository = require('./admin-repository.js');
 const VoterRepository = require('./voter-repository.js');
+const CandidateRepository = require('./candidate-repository.js');
+const ElectionRepository = require('./election-repository.js');
+const ElectionConfigurationRepository = require('./election-configuration-repository.js');
 
 function Repositories(mappers, configService) {
   this.countryRepository = new CountryRepository(mappers);
   this.adminRepository = new AdminRepository(mappers, configService);
   this.voterRepository = new VoterRepository(mappers, configService);
+  this.candidateRepository = new CandidateRepository(mappers, configService);
+  this.electionRepository = new ElectionRepository(mappers, configService);
+  this.electionConfigurationRepository = new ElectionConfigurationRepository(
+    mappers,
+    configService
+  );
 }
 
 module.exports = Repositories;
