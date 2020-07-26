3e4ca00ce8f1a39049a9a7d4f904a227cbedb7b2
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-26 15:35:36 +0530

    code refactor

--- app/repositories/election-repository.js
+++ app/repositories/election-repository.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { isEmpty, first } = require('lodash');
-const { selectQuery, insertQuery, updateQuery } = require('../functional/query.js');
+const { selectQuery, insertQuery, updateQuery, pagination } = require('../functional/query.js');
 const TableRepository = require('./table-repository.js');
 const tableRepository = new TableRepository();
 const T = tableRepository.tables();
@@ -50,10 +50,9 @@ function ElectionRepository(mappers, configService) {
     transaction
   ) {
     const result = await findBy({
+      ...pagination({ limit, page }),
       whereClause: { electionStatus },
       columnsToReturn,
-      limit,
-      offset: (page - 1) * limit,
       transaction
     });
     if (isEmpty(result)) return null;
