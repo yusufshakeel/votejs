aeccafad599143cd10390e4a8ceb68029042625d
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-07-26 23:27:05 +0530

    create, upsert - election config

---
+++ app/repositories/election-configuration-repository.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const { isEmpty, first } = require('lodash');
+const { selectQuery, insertQuery, updateQuery, pagination } = require('../functional/query.js');
+const TableRepository = require('./table-repository.js');
+const tableRepository = new TableRepository();
+const T = tableRepository.tables();
+
+const columnsToReturn = [
+  'guid',
+  'electionGuid',
+  'candidateGuid',
+  'electionConfigurationStatus',
+  'createdAt',
+  'updatedAt'
+];
+
+function ElectionConfigurationRepository(mappers, configService) {
+  const { electionConfigurationMapper } = mappers;
+  const { dbQueryLimit: DB_QUERY_LIMIT } = configService;
+
+  const findBy = params => selectQuery({ table: T.ELECTION_CONFIGURATION, ...params });
+
+  const find = async function ({ whereClause, transaction }) {
+    const result = await findBy({
+      ...pagination({ limit: 1, page: 1 }),
+      whereClause,
+      columnsToReturn,
+      transaction
+    });
+    if (isEmpty(result)) return null;
+    return electionConfigurationMapper.dbToDomain(first(result));
+  };
+
+  const finds = async function ({ whereClause, limit, page, transaction }) {
+    const result = await findBy({
+      ...pagination({ limit, page }),
+      whereClause,
+      columnsToReturn,
+      transaction
+    });
+    if (isEmpty(result)) return null;
+    return result.map(row => electionConfigurationMapper.dbToDomain(row));
+  };
+
+  this.create = async function (domainElectionConfiguration, transaction) {
+    const dbElectionConfiguration = electionConfigurationMapper.domainToDb(
+      domainElectionConfiguration
+    );
+    const result = await insertQuery({
+      table: T.ELECTION_CONFIGURATION,
+      dataToInsert: dbElectionConfiguration,
+      columnsToReturn,
+      transaction
+    });
+    return electionConfigurationMapper.dbToDomain(first(result));
+  };
+
+  this.findByGuid = function (guid, transaction) {
+    return find({ whereClause: { guid }, transaction });
+  };
+
+  this.findByElectionGuid = function (electionGuid, transaction) {
+    return find({ whereClause: { electionGuid }, transaction });
+  };
+
+  this.findByCandidateGuid = function (candidateGuid, transaction) {
+    return find({ whereClause: { candidateGuid }, transaction });
+  };
+
+  this.findByElectionConfigurationStatus = function (
+    { electionConfigurationStatus, limit = DB_QUERY_LIMIT, page = 1 },
+    transaction
+  ) {
+    return finds({ whereClause: { electionConfigurationStatus }, limit, page, transaction });
+  };
+
+  this.updateByGuid = async function (guid, domainElectionConfiguration, transaction) {
+    const fetchedElectionConfiguration = await this.findByGuid(guid, transaction);
+    if (isEmpty(fetchedElectionConfiguration)) return null;
+    const dataToUpdate = electionConfigurationMapper.updateDomainToDb(domainElectionConfiguration);
+    const result = await updateQuery({
+      table: T.ELECTION_CONFIGURATION,
+      dataToUpdate,
+      whereClause: { guid },
+      columnsToReturn,
+      transaction
+    });
+    return electionConfigurationMapper.dbToDomain(first(result));
+  };
+
+  this.upsert = async function (domainElectionConfiguration, transaction) {
+    const dbElectionConfiguration = electionConfigurationMapper.domainToDb(
+      domainElectionConfiguration
+    );
+    const foundElectionConfiguration = await this.findAll(
+      {
+        whereClause: {
+          electionGuid: dbElectionConfiguration.electionGuid,
+          candidateGuid: dbElectionConfiguration.candidateGuid
+        },
+        limit: 1
+      },
+      transaction
+    );
+    if (isEmpty(foundElectionConfiguration)) {
+      return this.create(domainElectionConfiguration, transaction);
+    }
+    return this.updateByGuid(
+      dbElectionConfiguration.guid,
+      domainElectionConfiguration,
+      transaction
+    );
+  };
+
+  this.findAll = function ({ whereClause, limit = DB_QUERY_LIMIT, page = 1 }, transaction) {
+    return finds({ whereClause, limit, page, transaction });
+  };
+}
+
+module.exports = ElectionConfigurationRepository;
---
+++ tests/integration/app/repositories/election-configuration-repository.integration.test.js
@@ -0,0 +1,186 @@
+'use strict';
+
+const { keys } = require('lodash');
+const Services = require('../../../../app/services');
+const CandidateRepository = require('../../../../app/repositories/candidate-repository.js');
+const ElectionRepository = require('../../../../app/repositories/election-repository.js');
+const ElectionConfigurationRepository = require('../../../../app/repositories/election-configuration-repository.js');
+const CandidateMapper = require('../../../../app/mappers/candidate-mapper.js');
+const ElectionMapper = require('../../../../app/mappers/election-mapper.js');
+const ElectionConfigurationMapper = require('../../../../app/mappers/election-configuration-mapper.js');
+
+const {
+  ELECTION_CONFIGURATION_STATUS_ACTIVE
+} = require('../../../../app/constants/election-configuration-constants.js');
+
+const { ELECTION_STATUS_PUBLIC } = require('../../../../app/constants/election-constants.js');
+
+const { CANDIDATE_STATUS_ACTIVE } = require('../../../../app/constants/candidate-constants.js');
+
+const services = new Services();
+const { configService, knexService, uuidService, timeService } = services;
+
+const now = timeService.now();
+const DB_QUERY_LIMIT = configService.dbQueryLimit;
+
+function FakeAuditMapper() {
+  this.updateDomainAudit = function () {
+    return { audit: { updatedAt: now } };
+  };
+}
+
+function FakeMappers() {
+  this.auditMapper = new FakeAuditMapper();
+  this.candidateMapper = new CandidateMapper(this.auditMapper);
+  this.electionMapper = new ElectionMapper(this.auditMapper);
+  this.electionConfigurationMapper = new ElectionConfigurationMapper(this.auditMapper);
+}
+
+const mappers = new FakeMappers();
+const candidateRepository = new CandidateRepository(mappers, configService);
+const electionRepository = new ElectionRepository(mappers, configService);
+const electionConfigurationRepository = new ElectionConfigurationRepository(mappers, configService);
+
+const getFakeDomainElection = (guid = uuidService.uuid()) => ({
+  guid,
+  title: `Election Title ${guid}`,
+  summary: `Election summary ${guid}`,
+  startsAt: now,
+  endsAt: now,
+  electionStatus: ELECTION_STATUS_PUBLIC,
+  electionSettings: {
+    field: 'value'
+  },
+  audit: {
+    createdAt: now
+  }
+});
+
+const getFakeDomainCandidate = (guid = uuidService.uuid()) => ({
+  guid,
+  candidateHandle: `Candidate Handle ${guid}`,
+  displayHeader: `Candidate display header ${guid}`,
+  summary: `Candidate summary ${guid}`,
+  candidateStatus: CANDIDATE_STATUS_ACTIVE,
+  audit: {
+    createdAt: now
+  }
+});
+
+const getFakeDomainElectionConfiguration = ({
+  guid = uuidService.uuid(),
+  electionGuid,
+  candidateGuid
+}) => ({
+  guid,
+  electionGuid,
+  candidateGuid,
+  electionConfigurationStatus: ELECTION_CONFIGURATION_STATUS_ACTIVE,
+  audit: {
+    createdAt: now
+  }
+});
+
+const getFakeDomainElectionConfigurationResponse = (guid, electionGuid, candidateGuid) => ({
+  guid,
+  electionGuid,
+  candidateGuid,
+  electionConfigurationStatus: ELECTION_CONFIGURATION_STATUS_ACTIVE,
+  audit: {
+    createdAt: now
+  }
+});
+
+const fakeDomainElections = [getFakeDomainElection(), getFakeDomainElection()];
+
+const fakeDomainCandidates = [
+  getFakeDomainCandidate(),
+  getFakeDomainCandidate(),
+  getFakeDomainCandidate()
+];
+
+const fakeDomainElectionConfigurations = [
+  // election config #1 -- consists of -- election #1, candidate #1
+  getFakeDomainElectionConfiguration({
+    electionGuid: fakeDomainElections[0].guid,
+    candidateGuid: fakeDomainCandidates[0].guid
+  }),
+  // election config #2 -- consists of -- election #1, candidate #2
+  getFakeDomainElectionConfiguration({
+    electionGuid: fakeDomainElections[0].guid,
+    candidateGuid: fakeDomainCandidates[1].guid
+  }),
+  // election config #3 -- consists of -- election #2, candidate #3
+  getFakeDomainElectionConfiguration({
+    electionGuid: fakeDomainElections[1].guid,
+    candidateGuid: fakeDomainCandidates[2].guid
+  })
+];
+
+beforeAll(() => {
+  return knexService.transaction(async txn => {
+    await Promise.all([
+      electionRepository.create(fakeDomainElections[0], txn),
+      electionRepository.create(fakeDomainElections[1], txn)
+    ]);
+    await Promise.all([
+      candidateRepository.create(fakeDomainCandidates[0], txn),
+      candidateRepository.create(fakeDomainCandidates[1], txn),
+      candidateRepository.create(fakeDomainCandidates[2], txn)
+    ]);
+  });
+});
+
+test('Should be able to create new election configuration', async () => {
+  return knexService.transaction(async txn => {
+    const electionConfigurations = await Promise.all([
+      electionConfigurationRepository.create(fakeDomainElectionConfigurations[0], txn),
+      electionConfigurationRepository.create(fakeDomainElectionConfigurations[1], txn),
+      electionConfigurationRepository.create(fakeDomainElectionConfigurations[2], txn)
+    ]);
+    electionConfigurations.forEach((electionConfiguration, index) => {
+      expect(electionConfiguration.electionGuid).toBe(
+        fakeDomainElectionConfigurations[index].electionGuid
+      );
+      expect(electionConfiguration.candidateGuid).toBe(
+        fakeDomainElectionConfigurations[index].candidateGuid
+      );
+      expect(fakeDomainElectionConfigurations[index]).toStrictEqual(
+        getFakeDomainElectionConfigurationResponse(
+          fakeDomainElectionConfigurations[index].guid,
+          fakeDomainElectionConfigurations[index].electionGuid,
+          fakeDomainElectionConfigurations[index].candidateGuid
+        )
+      );
+    });
+  });
+});
+
+test('Should be able to update existing election configuration - upsert', async () => {
+  return knexService.transaction(async txn => {
+    const electionConfigurations = await Promise.all([
+      electionConfigurationRepository.upsert(fakeDomainElectionConfigurations[0], txn),
+      electionConfigurationRepository.upsert(fakeDomainElectionConfigurations[1], txn),
+      electionConfigurationRepository.upsert(fakeDomainElectionConfigurations[2], txn)
+    ]);
+    electionConfigurations.forEach((electionConfiguration, index) => {
+      expect(electionConfiguration.electionGuid).toBe(
+        fakeDomainElectionConfigurations[index].electionGuid
+      );
+      expect(electionConfiguration.candidateGuid).toBe(
+        fakeDomainElectionConfigurations[index].candidateGuid
+      );
+      expect(fakeDomainElectionConfigurations[index]).toStrictEqual(
+        getFakeDomainElectionConfigurationResponse(
+          fakeDomainElectionConfigurations[index].guid,
+          fakeDomainElectionConfigurations[index].electionGuid,
+          fakeDomainElectionConfigurations[index].candidateGuid
+        )
+      );
+    });
+  });
+});
+
+afterAll(() => {
+  return knexService.destroy();
+});
