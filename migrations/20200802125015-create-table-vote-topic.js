3932e35ddbde9453d0746472621e97cdb0da37d7
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-08-02 18:26:11 +0530

    voteTopic table migration + constants

---
+++ migrations/20200802125015-create-table-vote-topic.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, cb) {
+  return db.createTable('VOTE_TOPIC', {
+    id: {
+      type: 'int',
+      unsigned: true,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    guid: {
+      type: 'uuid',
+      unique: true,
+      notNull: true,
+      defaultValue: new String('uuid_generate_v4()')
+    },
+    electionGuid: {
+      type: 'uuid',
+      notNull: true,
+      foreignKey: {
+        name: 'VOTE_TOPIC_electionGuid_ELECTION_guid_fk',
+        table: 'ELECTION',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        },
+        mapping: 'guid'
+      }
+    },
+    topicGuid: {
+      type: 'uuid',
+      notNull: true,
+      foreignKey: {
+        name: 'VOTE_TOPIC_topicGuid_TOPIC_guid_fk',
+        table: 'TOPIC',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        },
+        mapping: 'guid'
+      }
+    },
+    voterGuid: {
+      type: 'uuid',
+      notNull: true,
+      foreignKey: {
+        name: 'VOTE_TOPIC_voterGuid_VOTER_guid_fk',
+        table: 'VOTER',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        },
+        mapping: 'guid'
+      }
+    },
+    voteStatus: {
+      type: 'string',
+      notNull: true,
+      defaultValue: 'VALID'
+    },
+    createdAt: {
+      type: 'timestamptz',
+      notNull: true,
+      defaultValue: new String('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+      type: 'timestamptz'
+    }
+  }, cb);
+};
+
+exports.down = function (db, cb) {
+  return db.dropTable('VOTE_TOPIC', {}, cb);
+};
+
+exports._meta = {
+  'version': 1
+};
