1806906722982b360ba6fdf34b13d56134f86c08
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-08-06 12:08:05 +0530

    injecting passwordService to repositories

--- tests/unit/app/repositories/index.unit.test.js
+++ tests/unit/app/repositories/index.unit.test.js
@@ -4,11 +4,17 @@ const { keys } = require('lodash');
 const Repositories = require('../../../../app/repositories');
 const Mappers = require('../../../../app/mappers');
 const ConfigService = require('../../../../app/services/config-service.js');
+const PasswordService = require('../../../../app/services/password-service.js');
+const Base64Service = require('../../../../app/services/base64-service.js');
+const StringifyService = require('../../../../app/services/stringify-service.js');
 
 test('Should confirm the existence of required properties', () => {
   const mappers = new Mappers();
   const configService = ConfigService();
-  const repositories = new Repositories({ mappers, configService });
+  const base64Service = Base64Service();
+  const stringifyService = StringifyService();
+  const passwordService = new PasswordService(configService, base64Service, stringifyService);
+  const repositories = new Repositories({ mappers, configService, passwordService });
   expect(keys(repositories).sort()).toStrictEqual(
     [
       'countryRepository',
