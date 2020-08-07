b0d1fc76e585857d6e232dc02e20dd3cf56e26fd
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-08-07 21:35:41 +0530

    logService

--- app/services/log-service.js
+++ app/services/log-service.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const { pickBy } = require('lodash');
 const pinoLogger = require('pino');
 
 function LogService(configService, pino = pinoLogger) {
   const logger = pino({
     level: configService.logLevel
   });
+  const logFn = fn => (module, functionName, message, details) =>
+    fn(pickBy({ module, functionName, message, details }));
   return {
     logger,
+    logINFO: logFn(logger.info),
+    logSUCCESS: logFn(logger.success),
+    logERROR: logFn(logger.error),
+    logVERBOSE: logFn(logger.verbose),
     INFO: param => logger.info(param),
     SUCCESS: param => logger.success(param),
     ERROR: param => logger.error(param),
