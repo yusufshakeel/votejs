12a6bb058bf20f2fb216a905ecad44db468cec6e
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-08-08 12:53:56 +0530

    adminDispatcher

--- app/dispatchers/admin-dispatcher.js
+++ app/dispatchers/admin-dispatcher.js
@@ -12,6 +12,55 @@ function AdminDispatcher({ mappers, services, repositories }) {
       return adminMapper.domainToApi(createdAdmin);
     });
   };
+
+  this.findAdminByGuid = function (guid) {
+    return knexService.transaction(async txn => {
+      const fetchedAdmin = await adminRepository.findByGuid(guid, txn);
+      return adminMapper.domainToApi(fetchedAdmin);
+    });
+  };
+
+  this.findAdminByEmailId = function (emailId) {
+    return knexService.transaction(async txn => {
+      const fetchedAdmin = await adminRepository.findByEmailId(emailId, txn);
+      return adminMapper.domainToApi(fetchedAdmin);
+    });
+  };
+
+  this.findAdminByUserName = function (userName) {
+    return knexService.transaction(async txn => {
+      const fetchedAdmin = await adminRepository.findByUserName(userName, txn);
+      return adminMapper.domainToApi(fetchedAdmin);
+    });
+  };
+
+  this.findAdminByAccountStatus = function ({ accountStatus, limit, page }) {
+    return knexService.transaction(async txn => {
+      const fetchedAdmins = await adminRepository.findByAccountStatus(
+        { accountStatus, limit, page },
+        txn
+      );
+      return fetchedAdmins.map(fetchedAdmin => adminMapper.domainToApi(fetchedAdmin));
+    });
+  };
+
+  this.updateAdmin = function (adminGuid, apiAdmin) {
+    return knexService.transaction(async txn => {
+      const domainAdmin = adminMapper.apiToDomain(apiAdmin);
+      const updatedAdmin = await adminRepository.updateByGuid(adminGuid, domainAdmin, txn);
+      return adminMapper.domainToApi(updatedAdmin);
+    });
+  };
+
+  this.loginAdmin = function ({ userName, emailId, password, passcode }) {
+    return knexService.transaction(async txn => {
+      const fetchedAdmin = await adminRepository.validateForLogin(
+        { userName, emailId, password, passcode },
+        txn
+      );
+      return adminMapper.domainToApi(fetchedAdmin);
+    });
+  };
 }
 
 module.exports = AdminDispatcher;
--- tests/unit/app/dispatchers/admin-dispatcher.unit.test.js
+++ tests/unit/app/dispatchers/admin-dispatcher.unit.test.js
@@ -25,3 +25,130 @@ test('Should be able to create admin', async () => {
   });
   expect(repositories.adminRepository.create.mock.calls.length).toBe(1);
 });
+
+test('Should be able to fetch admin by guid', async () => {
+  const repositories = {
+    adminRepository: {
+      findByGuid: jest.fn(() => {
+        return {
+          guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+        };
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.findAdminByGuid('9e17d7b7-c236-496f-92cd-10e1859fdd3b');
+  expect(repositories.adminRepository.findByGuid.mock.calls.length).toBe(1);
+});
+
+test('Should be able to fetch admin by emailId', async () => {
+  const repositories = {
+    adminRepository: {
+      findByEmailId: jest.fn(() => {
+        return {
+          guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+        };
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.findAdminByEmailId('janedoe@example.com');
+  expect(repositories.adminRepository.findByEmailId.mock.calls.length).toBe(1);
+});
+
+test('Should be able to fetch admin by userName', async () => {
+  const repositories = {
+    adminRepository: {
+      findByUserName: jest.fn(() => {
+        return {
+          guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+        };
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.findAdminByUserName('janedoe');
+  expect(repositories.adminRepository.findByUserName.mock.calls.length).toBe(1);
+});
+
+test('Should be able to fetch admin by accountStatus', async () => {
+  const repositories = {
+    adminRepository: {
+      findByAccountStatus: jest.fn(() => {
+        return [
+          {
+            guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+          }
+        ];
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.findAdminByAccountStatus('ACTIVE');
+  expect(repositories.adminRepository.findByAccountStatus.mock.calls.length).toBe(1);
+});
+
+test('Should be able to update admin by guid', async () => {
+  const repositories = {
+    adminRepository: {
+      updateByGuid: jest.fn(() => {
+        return {
+          guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+        };
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.updateAdmin('9e17d7b7-c236-496f-92cd-10e1859fdd3b', { firstName: 'Jane' });
+  expect(repositories.adminRepository.updateByGuid.mock.calls.length).toBe(1);
+});
+
+test('Should be able to validate admin login', async () => {
+  const repositories = {
+    adminRepository: {
+      validateForLogin: jest.fn(() => {
+        return {
+          guid: '9e17d7b7-c236-496f-92cd-10e1859fdd3b'
+        };
+      })
+    }
+  };
+  const services = {
+    knexService: {
+      transaction: async f => f()
+    }
+  };
+  const adminDispatcher = new AdminDispatcher({ mappers, services, repositories });
+  await adminDispatcher.loginAdmin({
+    userName: 'janedoe',
+    emailId: 'janedoe@example.com',
+    password: 'root1234',
+    passcode: '123456'
+  });
+  expect(repositories.adminRepository.validateForLogin.mock.calls.length).toBe(1);
+});
